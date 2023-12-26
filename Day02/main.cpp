05c474eda23ddc158df5c31b782beaad6a012056
Jenn Hernes <jenn_hernes@hotmail.com>
2023-12-26 15:05:39 +0200

    faster day 2

--- Day02/main.cpp
+++ Day02/main.cpp
@@ -4,46 +4,53 @@
 #include <fstream>
 #include <iostream>
 
-int main() {
-    std::ifstream file { "../input.txt" };
-    std::string line {};
-    int sum {};
-    int product {};
-    int id {};
-    std::string tmp {};
-    while (std::getline(file, line)) {
+const int RED{0};
+const int GREEN{1};
+const int BLUE{2};
+
+int main()
+{
+    std::ifstream file{"../input.txt"};
+    std::string line{};
+    std::stringstream ss;
+    int sum{};
+    int product{};
+    int id{};
+    std::array<int, 3> maxCubes{};
+    int count{};
+    while (std::getline(file, line))
+    {
         if (line.empty()) continue;
+        ss.clear();
+        ss << line;
 
-        line.append(";");
-        std::array<int,3> minCubes {};
-        std::stringstream ss{line};
-
-        getline(ss, tmp, ' '); // skip "Game"
-        int count {};
-        while(getline(ss, tmp, ' ')) {
-            if (tmp.find(':') != std::string::npos) {
-                id = stoi(tmp.substr(0, tmp.size()-1));
-                // std::cout << id << ": ";
-            } else if (tmp.find(',') != std::string::npos || tmp.find(';') != std::string::npos) {
-                std::string colour {tmp.substr(0, tmp.size()-1)};
-                // std::cout << count << " " << colour << " ";
-                if (colour == "green") {
-                    minCubes[0] = std::max(minCubes[0], count);
-                } else if (colour == "red") {
-                    minCubes[1] = std::max(minCubes[1], count);
-                } else if (colour == "blue") {
-                    minCubes[2] = std::max(minCubes[2], count);
-                }
-            } else {
-                count = stoi(tmp);
+        ss >> line; // skip "Game"
+        ss >> line;
+        id = stoi(line.substr(0, line.size() - 1));
+        count = 0;
+        maxCubes[RED] = 0;
+        maxCubes[GREEN] = 0;
+        maxCubes[BLUE] = 0;
+        while (ss >> count)
+        {
+            ss >> line;
+            if (line.starts_with("red"))
+            {
+                maxCubes[RED] = std::max(maxCubes[RED], count);
+            } else if (line.starts_with("blue"))
+            {
+                maxCubes[BLUE] = std::max(maxCubes[BLUE], count);
+            } else if (line.starts_with("green"))
+            {
+                maxCubes[GREEN] = std::max(maxCubes[GREEN], count);
             }
         }
-        // std::cout << '\n';
-        if (minCubes[0] <= 13 && minCubes[1] <= 12 && minCubes[2] <= 14) {
+        if (maxCubes[RED] <= 12 && maxCubes[GREEN] <= 13 && maxCubes[BLUE] <= 14)
+        {
             sum += id;
         }
-        product += minCubes[0] * minCubes[1] * minCubes[2];
+        product += maxCubes[GREEN] * maxCubes[RED] * maxCubes[BLUE];
     }
 
-    std::cout << sum << " " << product << std::endl;
+    std::cout << sum << " " << product << '\n';
 }
