6c59d24df356a2805b81b78187f50a11ca4630f3
Jenn Hernes <jenn_hernes@hotmail.com>
2023-12-27 21:45:30 +0200

    add solution for day 10 part 2

--- Day10/main.cpp
+++ Day10/main.cpp
@@ -1,99 +1,130 @@
+#include <algorithm>
 #include <array>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <vector>
 
-struct Pos
-{
-    int x{};
-    int y{};
-
-    Pos operator+(Pos other) const
-    {
-        return Pos{this->x + other.x, this->y + other.y};
-    }
-};
 
 int main()
 {
-    std::ifstream file{"../test.txt"};
+    std::ifstream file{"../input.txt"};
     std::string line;
     std::stringstream ss;
 
     std::vector<std::vector<char>> grid{};
-    std::vector<Pos> ground{};
-    std::vector<std::array<Pos, 2>> loop{};
+    std::vector<char> row{};
     int x;
     int y{};
     int sx{};
     int sy{};
     while (std::getline(file, line))
     {
-        x = 0;
-        std::vector<char> row{};
-        for (const auto c: line)
+        if (sx == 0 && sy == 0)
         {
-            if (c == 'S')
+            x = 0;
+            for (const auto c: line)
             {
-                sx = x;
-                sy = y;
-            } else if (c == '.')
-            {
-                ground.push_back({x, y});
+                if (c == 'S')
+                {
+                    sx = x;
+                    sy = y;
+                    break;
+                }
+                ++x;
             }
-            row.push_back(c);
-            ++x;
+            ++y;
         }
-        grid.push_back(row);
-        ++y;
+        grid.emplace_back(line.begin(), line.end());
     }
 
-    std::cout << grid.size() << " " << sx << " " << sy << '\n';
-    Pos direction{0, 0};
+    std::array<int, 2> direction{0, 0};
     if (sx < grid.size() - 1 && (grid[sy][sx + 1] == '-' || grid[sy][sx + 1] == 'J' || grid[sy][sx + 1] == '7'))
     {
-        direction.x = 1;
+        direction[1] = 1;
     } else if (sy < grid.size() - 1 && (grid[sy + 1][sx] == '|' || grid[sy + 1][sx] == 'L' || grid[sy + 1][sx] == 'J'))
     {
-        direction.y = 1;
-    } else if (sx > 0)
-    {
-        direction.x = -1;
+        direction[0] = 1;
     } else
     {
-        direction.y = -1;
+        direction[1] = -1;
     }
 
-    Pos currentPos{sx + direction.x, sy + direction.y};
+    std::map<int, std::vector<int>> loop{};
+    std::array<int, 2> currentPos{sy + direction[0], sx + direction[1]};
     int length{1};
-    while (grid[currentPos.y][currentPos.x] != 'S')
+    while (grid[currentPos[0]][currentPos[1]] != 'S')
     {
-        loop.push_back(std::array<Pos, 2>{Pos{currentPos.x, currentPos.y}, Pos{direction.x, direction.y}});
-        switch (grid[currentPos.y][currentPos.x])
+        if (!loop.contains(currentPos[0])) loop[currentPos[0]] = {};
+        loop[currentPos[0]].push_back(currentPos[1]);
+        switch (grid[currentPos[0]][currentPos[1]])
         {
             case 'L':
             case '7':
             {
-                int tmp = direction.x;
-                direction.x = direction.y;
-                direction.y = tmp;
+                int tmp = direction[1];
+                direction[1] = direction[0];
+                direction[0] = tmp;
                 break;
             }
             case 'J':
             case 'F':
             {
-                int tmp = direction.x;
-                direction.x = direction.y * -1;
-                direction.y = tmp * -1;
+                int tmp = direction[1];
+                direction[1] = direction[0] * -1;
+                direction[0] = tmp * -1;
                 break;
             }
             default: break;
         }
-        currentPos.x += direction.x;
-        currentPos.y += direction.y;
+        currentPos[1] += direction[1];
+        currentPos[0] += direction[0];
         length++;
     }
 
     std::cout << length / 2 << '\n';
+
+    int previousCorner{};
+    bool inRegion;
+    int total{};
+    for (int i{}; i < grid.size(); ++i)
+    {
+        inRegion = false;
+        for (int j{}; j < grid[i].size(); ++j)
+        {
+            if (grid[i][j] == 'S' || (loop.contains(i) && std::find(loop.at(i).begin(), loop.at(i).end(), j) != loop.
+                                      at(i).end()))
+            {
+                if (grid[i][j] == 'F' || grid[i][j] == '7') // || grid[i][j] == 'S')
+                {
+                    if (previousCorner == 1) previousCorner = 0;
+                    else if (previousCorner == 0) previousCorner = 1;
+                    else
+                    {
+                        inRegion = !inRegion;
+                        previousCorner = 0;
+                    }
+                } else if (grid[i][j] == 'L' || grid[i][j] == 'J' || grid[i][j] == 'S')
+                {
+                    if (previousCorner == -1) previousCorner = 0;
+                    else if (previousCorner == 0) previousCorner = -1;
+                    else
+                    {
+                        inRegion = !inRegion;
+                        previousCorner = 0;
+                    }
+                } else if (grid[i][j] == '|')
+                {
+                    inRegion = !inRegion;
+                    previousCorner = 0;
+                }
+            } else if (inRegion)
+            {
+                ++total;
+            }
+        }
+    }
+
+    std::cout << total << '\n';
 }
